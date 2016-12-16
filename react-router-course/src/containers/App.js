dd17a3af1cef60a6fc68bcc45f38e1ca681f01ef
Dmitry Turovtsov <web.best.master@gmail.com>
2016-12-16 19:06:39 +0300

    react-router-course - in progress...

--- react-router-course/src/containers/App.js
+++ react-router-course/src/containers/App.js
@@ -1,7 +1,17 @@
-import React, { Component } from 'react'
-
+import React, {Component} from 'react'
+import {Link} from 'react-router'
 export default class App extends Component {
     render() {
-        return <div className='container'>Привет из App!</div>
+        return (
+            <div className='container'>
+                <h1>App</h1>
+                <ul>
+                    <li><Link to='/admin'>Admin</Link></li>
+                    <li><Link to='/genre'>Genre</Link></li>
+                </ul>
+                {/* добавили вывод потомков */}
+                {this.props.children}
+            </div>
+        )
     }
 }
--- react-router-course/src/index.js
+++ react-router-course/src/index.js
@@ -1,9 +1,24 @@
 import 'babel-polyfill'
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 import App from './containers/App'
+import Admin from './components/Admin'
+import Genre from './components/Genre'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import { Router, Route, IndexRoute,
+    browserHistory
+    // hashHistory
+} from 'react-router'
 
 render(
-    <App />,
+    <Router history={browserHistory}>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home} />
+            <Route path='admin' component={Admin} />
+            <Route path='genre' component={Genre} />
+        </Route>
+        <Route path='*' component={NotFound} />
+    </Router>,
     document.getElementById('root')
-)
+);
