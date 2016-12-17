93a3cb15387ba1abfafa5b6f3a56b6770e4b9b48
Dmitry Turovtsov <web.best.master@gmail.com>
2016-12-17 14:45:32 +0300

    react-router-course - in progress...

--- react-router-course/src/containers/App.js
+++ react-router-course/src/containers/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import NavLink from '../components/NavLink'
+
+import './styles.scss'
+
 export default class App extends Component {
     render() {
         return (
             <div className='container'>
                 <ul className='nav nav-pills'>
+                    <li><NavLink onlyActiveOnIndex={true} to='/'>Главная</NavLink></li>
                     <li><Link to='/admin'>Админка</Link></li>
-                    <li><Link to='/list'>Список жанров</Link></li>
+                    <li><NavLink to='/list'>Список жанров</NavLink></li>
+                    <li><Link to='/list' activeClassName='active'>Список жанров</Link></li>
                 </ul>
                 {this.props.children}
             </div>
